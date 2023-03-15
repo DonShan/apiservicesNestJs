46cd735cb899796c2460795227bda4d1995a1478
DonShan <msdjshan47@gmail.com>
2023-03-15 13:07:37 +0530

    updated all with prettier code formatter

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,38 +1,43 @@
-import { Body, Controller, ForbiddenException, Get, Param, ParseIntPipe, Post, Render} from "@nestjs/common";
-import { AuthService } from "./auth.service";
-import { AuthDto } from "./dto/auth.dto";
+import {
+  Body,
+  Controller,
+  ForbiddenException,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Render,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto/auth.dto';
 
-@Controller("auth")
+@Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService){
-
-    }
-    @Post("signup")
-   signup(@Body() dto: AuthDto){
+  constructor(private authService: AuthService) {}
+  @Post('signup')
+  signup(@Body() dto: AuthDto) {
     console.log({
-        dto,
+      dto,
     });
     return this.authService.signup(dto);
-   }
+  }
 
-    @Post("signin")
-    signin(@Body() dto: AuthDto) {
-        console.log({
-            dto,
-        });
-        return this.authService.signin(dto);
-    }
+  @Post('signin')
+  signin(@Body() dto: AuthDto) {
+    console.log({
+      dto,
+    });
+    return this.authService.signin(dto);
+  }
 
-    @Post("forgot-password")
+  @Post('forgot-password')
   async forgotPassword(@Body() dto: { email: string }) {
     console.log(dto);
     return this.authService.sendPasswordResetEmail(dto.email);
   }
 
   @Post('reset-password')
-async resetPassword(@Body() dto: { resetToken: string, password: string }) {
-  return this.authService.resetPassword(dto.resetToken, dto.password);
-}
-
-
+  async resetPassword(@Body() dto: { resetToken: string; password: string }) {
+    return this.authService.resetPassword(dto.resetToken, dto.password);
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,179 +1,191 @@
-import { ForbiddenException, Injectable, InternalServerErrorException } from "@nestjs/common";
-import { PrismaService } from "src/prisma/prisma.service";
-import { AuthDto } from "./dto/auth.dto";
+import {
+  ForbiddenException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto/auth.dto';
 import * as argon from 'argon2';
-import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
-import { JwtService } from "@nestjs/jwt";
-import { ConfigService } from "@nestjs/config";
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 import * as nodemailer from 'nodemailer';
 
 @Injectable()
-export class AuthService{
-constructor (private prisma: PrismaService, private jwt: JwtService, private config: ConfigService){}
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
 
-   async signup(dto: AuthDto){
+  async signup(dto: AuthDto) {
     //generate the password hash
     const hash = await argon.hash(dto.password);
 
     // generate password reset token
-    const resetToken = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-    
+    const resetToken =
+      Math.random().toString(36).substring(2, 15) +
+      Math.random().toString(36).substring(2, 15);
+
     // set password reset token expiration time
     const resetTokenExpires = new Date(Date.now() + 3600000); // token will expire in 1 hour
-    
+
     // save new user to db
-    try{
-        const user = await this.prisma.user.create({
-            data: {
-                email: dto.email,
-                hash,
-                 resetToken,
-                 resetTokenExpires,
-            }
-        });
-        console.log(user);
-  
-    } catch(error){
-        if (error instanceof PrismaClientKnownRequestError){
-            if (error.code === "P2002"){
-                throw new ForbiddenException(
-                    'Credentials taken', 
-                );
-                console.log(error.code);
-            }  
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+          resetToken,
+          resetTokenExpires,
+        },
+      });
+      console.log(user);
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+          console.log(error.code);
         }
-        //throw error;
-    } 
-   }
-   //find exsisting user from db
-    async signin(dto: AuthDto) {
-
-        const user = await this.prisma.user.findUnique({
-            where: {
-                email: dto.email,
-
-            },
-        });
-
-        if(!user) throw new ForbiddenException("Credentials incorrect");
-
-        const pwMatches = await argon.verify(user.hash, dto.password);
-
-        if(!pwMatches) throw new ForbiddenException("Credentials incorrect");
-
-        return this.signToken(user.id, user.email);
-            
+      }
+      //throw error;
     }
-    //Jwt token genaration
-    async signToken(userId: number, email: string): Promise<{access_token: string}>{
-        const payload = {
-            sub: userId,
-            email,
-        };
+  }
+  //find exsisting user from db
+  async signin(dto: AuthDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
 
-        const secret = this.config.get("JWT_SECRET");
-        
-        
-        const token = await this.jwt.signAsync(payload, {
-            expiresIn: "15m",
-            secret: "secret",
-        });
+    if (!user) throw new ForbiddenException('Credentials incorrect');
 
-        return {
-            access_token: token,
-        };
-    } 
-    //send password reset email 
-    async sendPasswordResetEmail(email: string) {
-        const resetToken = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-        const resetTokenExpires = new Date();
-        resetTokenExpires.setMinutes(resetTokenExpires.getMinutes() + 15);
-      
-        await this.prisma.user.update({
-          where: { email },
-          data: { resetToken, resetTokenExpires },
-        });
-        //email genarater nodemalier
-        const nodemailer = require('nodemailer');
+    const pwMatches = await argon.verify(user.hash, dto.password);
 
-        const transporter = nodemailer.createTransport({
-          host: 'smtp.gmail.com',
-          port: 587,
-          secure: false,
-          auth: {
-            user: 'msdjshan47@gmail.com',
-            pass: 'hdkbzcgcsfdvsgjy'
-          },
-          tls: {
-            rejectUnauthorized: true
-          },
-          authMethod: 'PLAIN'
-        });
-      
-        const mailOptions = {
-            to: email,
-            subject: "Password Reset",
-            text: `Please click on the following link to reset your password: http://localhost:3000/reset-password?resetToken=${resetToken}`,
-          };
-      
-          transporter.sendMail({
-            from: 'msdjshan47@gmail.com',
-            to: email,
-            subject: 'Test Email',
-            text: `Please click on the following link to reset your password: http://localhost:3300/reset-password?resetToken=${resetToken}`,
-          }, (error, info) => {
-            if (error) {
-              console.log(error);
-            } else {
-              console.log('Email sent: ' + info.response);
-            }
-          });
-      }
-      async resetPassword(resetToken: string, password: string) {
-        try {
-          // Find the user with the matching reset token
-          const user = await this.prisma.user.findUnique({
-            where: {
-              resetToken,
-            },
-          });
-        
-          if (!user) {
-            throw new ForbiddenException('Invalid reset token');
-          }
-        
-          // Check if the reset token has expired
-          const now = new Date();
-          if (user.resetTokenExpires < now) {
-            throw new ForbiddenException('Reset token has expired');
-          }
-        
-          // Hash the new password
-          const hash = await argon.hash(password);
-        
-          // Update the user's password hash and clear the reset token
-          const updatedUser = await this.prisma.user.update({
-            where: {
-              id: user.id,
-            },
-            data: {
-              hash,
-              resetToken: resetToken,
-              resetTokenExpires: null,
-            },
-          });
-        
-          return this.signToken(updatedUser.id, updatedUser.email);
-        } catch (error) {
-          if (error instanceof ForbiddenException) {
-            throw error;
-          }
-          throw new InternalServerErrorException();
+    if (!pwMatches) throw new ForbiddenException('Credentials incorrect');
+
+    return this.signToken(user.id, user.email);
+  }
+  //Jwt token genaration
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+
+    const secret = this.config.get('JWT_SECRET');
+
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret: 'secret',
+    });
+
+    return {
+      access_token: token,
+    };
+  }
+  //send password reset email
+  async sendPasswordResetEmail(email: string) {
+    const resetToken =
+      Math.random().toString(36).substring(2, 15) +
+      Math.random().toString(36).substring(2, 15);
+    const resetTokenExpires = new Date();
+    resetTokenExpires.setMinutes(resetTokenExpires.getMinutes() + 15);
+
+    await this.prisma.user.update({
+      where: { email },
+      data: { resetToken, resetTokenExpires },
+    });
+    //email genarater nodemalier
+
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 587,
+      secure: false,
+      auth: {
+        user: 'msdjshan47@gmail.com',
+        pass: 'hdkbzcgcsfdvsgjy',
+      },
+      tls: {
+        rejectUnauthorized: true,
+      },
+      authMethod: 'PLAIN',
+    });
+
+    const mailOptions = {
+      to: email,
+      subject: 'Password Reset',
+      text: `Please click on the following link to reset your password: http://localhost:3000/reset-password?resetToken=${resetToken}`,
+    };
+
+    transporter.sendMail(
+      {
+        from: 'msdjshan47@gmail.com',
+        to: email,
+        subject: 'Test Email',
+        text: `Please click on the following link to reset your password: http://localhost:3300/reset-password?resetToken=${resetToken}`,
+      },
+      (error, info) => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
         }
-      }
-      
-          
+      },
+    );
+  }
+  async resetPassword(resetToken: string, password: string) {
+    // generate password reset token
+    const resetTokenNew =
+      Math.random().toString(36).substring(2, 15) +
+      Math.random().toString(36).substring(2, 15);
 
-    
-    
+    // set password reset token expiration time
+    const resetTokenExpiresNew = new Date(Date.now() + 3600000); // token will expire in 1 hour
+    try {
+      // Find the user with the matching reset token
+      const user = await this.prisma.user.findUnique({
+        where: {
+          resetToken,
+        },
+      });
+
+      if (!user) {
+        throw new ForbiddenException('Invalid reset token');
+      }
+
+      // Check if the reset token has expired
+      const now = new Date();
+      if (user.resetTokenExpires < now) {
+        throw new ForbiddenException('Reset token has expired');
+      }
+
+      // Hash the new password
+      const hash = await argon.hash(password);
+
+      // Update the user's password hash and clear the reset token
+      const updatedUser = await this.prisma.user.update({
+        where: {
+          id: user.id,
+        },
+        data: {
+          hash,
+          resetToken: resetTokenNew,
+          resetTokenExpires: resetTokenExpiresNew,
+        },
+      });
+
+      return this.signToken(updatedUser.id, updatedUser.email);
+    } catch (error) {
+      if (error instanceof ForbiddenException) {
+        throw error;
+      }
+      throw new InternalServerErrorException();
+    }
+  }
 }
